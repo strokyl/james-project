163ce7b61e676d04618424406bb0c723bf0da804
Antoine Duprat <aduprat@linagora.com>
2017-01-24 10:17:45 +0100

    JAMES-1914 Factorize testing tools in a new project

--- server/testing/src/main/java/org/apache/james/utils/IMAPMessageReader.java
+++ server/testing/src/main/java/org/apache/james/utils/IMAPMessageReader.java
@@ -17,7 +17,7 @@
  * under the License.                                           *
  ****************************************************************/
 
-package org.apache.james.mailets.utils;
+package org.apache.james.utils;
 
 import java.io.Closeable;
 import java.io.IOException;
