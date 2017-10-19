1cd60e1a46208bef49c955398942393bb9df3bde
benwa <btellier@linagora.com>
2017-10-19 16:09:39 +0700

    JAMES-2195 JMAP integration tests for messages updates

--- server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/java/org/apache/james/jmap/methods/integration/cucumber/SetMessagesMethodStepdefs.java
+++ server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/java/org/apache/james/jmap/methods/integration/cucumber/SetMessagesMethodStepdefs.java
@@ -19,18 +19,28 @@
 
 package org.apache.james.jmap.methods.integration.cucumber;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
-import org.apache.http.entity.ContentType;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.james.mailbox.model.MailboxConstants;
 import org.apache.james.mailbox.model.MailboxId;
 import org.apache.james.mailbox.model.MessageId;
 import org.apache.james.modules.MailboxProbeImpl;
 
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.runtime.java.guice.ScenarioScoped;
 
@@ -41,6 +51,9 @@ public class SetMessagesMethodStepdefs {
     private final UserStepdefs userStepdefs;
     private final GetMessagesMethodStepdefs getMessagesMethodStepdefs;
 
+    private HttpResponse response;
+    private DocumentContext jsonPath;
+
     @Inject
     private SetMessagesMethodStepdefs(MainStepdefs mainStepdefs, UserStepdefs userStepdefs, GetMessagesMethodStepdefs getMessagesMethodStepdefs) {
         this.mainStepdefs = mainStepdefs;
@@ -48,16 +61,20 @@ public class SetMessagesMethodStepdefs {
         this.getMessagesMethodStepdefs = getMessagesMethodStepdefs;
     }
 
-    @When("^the user move \"([^\"]*)\" to mailbox \"([^\"]*)\"")
+    @When("^\"([^\"]*)\" move \"([^\"]*)\" to user mailbox \"([^\"]*)\"")
+    public void moveMessageToMailboxWithUser(String username, String message, String mailbox) throws Throwable {
+        userStepdefs.execWithUser(username, () -> moveMessageToMailbox(message, mailbox));
+    }
+
+    @When("^the user move \"([^\"]*)\" to user mailbox \"([^\"]*)\"")
     public void moveMessageToMailbox(String message, String mailbox) throws Throwable {
-        String username = userStepdefs.getConnectedUser();
         MessageId messageId = getMessagesMethodStepdefs.getMessageId(message);
         MailboxId mailboxId = mainStepdefs.jmapServer
             .getProbe(MailboxProbeImpl.class)
             .getMailbox(MailboxConstants.USER_NAMESPACE, userStepdefs.getConnectedUser(), mailbox)
             .getMailboxId();
 
-        String requestBody = "[" +
+        post("[" +
             "  [" +
             "    \"setMessages\","+
             "    {" +
@@ -67,18 +84,17 @@ public class SetMessagesMethodStepdefs {
             "    }," +
             "    \"#0\"" +
             "  ]" +
-            "]";
-        Request.Post(mainStepdefs.baseUri().setPath("/jmap").build())
-            .addHeader("Authorization", userStepdefs.getTokenForUser(username).serialize())
-            .bodyString(requestBody, ContentType.APPLICATION_JSON)
-            .execute()
-            .discardContent();
+            "]");
         mainStepdefs.awaitMethod.run();
+    }
+
+    @When("^\"([^\"]*)\" copy \"([^\"]*)\" from mailbox \"([^\"]*)\" to mailbox \"([^\"]*)\"")
+    public void copyMessageToMailbox(String username, String message, String sourceMailbox, String destinationMailbox) throws Throwable {
+        userStepdefs.execWithUser(username, () -> copyMessageToMailbox(message, sourceMailbox, destinationMailbox));
     }
 
     @When("^the user copy \"([^\"]*)\" from mailbox \"([^\"]*)\" to mailbox \"([^\"]*)\"")
     public void copyMessageToMailbox(String message, String sourceMailbox, String destinationMailbox) throws Throwable {
-        String username = userStepdefs.getConnectedUser();
         MessageId messageId = getMessagesMethodStepdefs.getMessageId(message);
         MailboxId sourceMailboxId = mainStepdefs.jmapServer
             .getProbe(MailboxProbeImpl.class)
@@ -89,7 +105,7 @@ public class SetMessagesMethodStepdefs {
             .getMailbox(MailboxConstants.USER_NAMESPACE, userStepdefs.getConnectedUser(), destinationMailbox)
             .getMailboxId();
 
-        String requestBody = "[" +
+        post("[" +
             "  [" +
             "    \"setMessages\","+
             "    {" +
@@ -99,40 +115,112 @@ public class SetMessagesMethodStepdefs {
             "    }," +
             "    \"#0\"" +
             "  ]" +
-            "]";
-        Request.Post(mainStepdefs.baseUri().setPath("/jmap").build())
-            .addHeader("Authorization", userStepdefs.getTokenForUser(username).serialize())
-            .bodyString(requestBody, ContentType.APPLICATION_JSON)
-            .execute()
-            .discardContent();
+            "]");
         mainStepdefs.awaitMethod.run();
+    }
+
+    @When("^\"([^\"]*)\" copy \"([^\"]*)\" from mailbox \"([^\"]*)\" of user \"([^\"]*)\" to mailbox \"([^\"]*)\" of user \"([^\"]*)\"")
+    public void copyMessageToMailbox(String username, String message, String sourceMailbox, String sourceUser, String destinationMailbox, String destinationUser) throws Throwable {
+        userStepdefs.execWithUser(username, () -> copyMessageToMailbox(message, sourceMailbox, sourceUser, destinationMailbox, destinationUser));
+    }
+
+    @When("^the user copy \"([^\"]*)\" from mailbox \"([^\"]*)\" of user \"([^\"]*)\" to mailbox \"([^\"]*)\" of user \"([^\"]*)\"")
+    public void copyMessageToMailbox(String message, String sourceMailbox, String sourceUser, String destinationMailbox, String destinationUser) throws Throwable {
+        MessageId messageId = getMessagesMethodStepdefs.getMessageId(message);
+        MailboxId sourceMailboxId = mainStepdefs.jmapServer
+            .getProbe(MailboxProbeImpl.class)
+            .getMailbox(MailboxConstants.USER_NAMESPACE, sourceUser, sourceMailbox)
+            .getMailboxId();
+        MailboxId destinationMailboxId = mainStepdefs.jmapServer
+            .getProbe(MailboxProbeImpl.class)
+            .getMailbox(MailboxConstants.USER_NAMESPACE, destinationUser, destinationMailbox)
+            .getMailboxId();
+
+        post("[" +
+            "  [" +
+            "    \"setMessages\","+
+            "    {" +
+            "      \"update\": { \"" + messageId.serialize() + "\" : {" +
+            "        \"mailboxIds\": [\"" + destinationMailboxId.serialize() + "\",\"" + sourceMailboxId.serialize() + "\"]" +
+            "      }}" +
+            "    }," +
+            "    \"#0\"" +
+            "  ]" +
+            "]");
+        mainStepdefs.awaitMethod.run();
+    }
+
+    @Given("^\"([^\"]*)\" move \"([^\"]*)\" to mailbox \"([^\"]*)\" of user \"([^\"]*)\"")
+    public void moveMessageToMailbox(String username, String message, String destinationMailbox, String destinationUser) throws Throwable {
+        userStepdefs.execWithUser(username, () -> moveMessageToMailbox(message, destinationMailbox, destinationUser));
+    }
+
+    @Given("^the user move \"([^\"]*)\" to mailbox \"([^\"]*)\" of user \"([^\"]*)\"")
+    public void moveMessageToMailbox(String message, String destinationMailbox, String destinationUser) throws Throwable {
+        MessageId messageId = getMessagesMethodStepdefs.getMessageId(message);
+        MailboxId destinationMailboxId = mainStepdefs.jmapServer
+            .getProbe(MailboxProbeImpl.class)
+            .getMailbox(MailboxConstants.USER_NAMESPACE, destinationUser, destinationMailbox)
+            .getMailboxId();
+
+        post("[" +
+            "  [" +
+            "    \"setMessages\","+
+            "    {" +
+            "      \"update\": { \"" + messageId.serialize() + "\" : {" +
+            "        \"mailboxIds\": [\"" + destinationMailboxId.serialize() + "\"]" +
+            "      }}" +
+            "    }," +
+            "    \"#0\"" +
+            "  ]" +
+            "]");
+        mainStepdefs.awaitMethod.run();
+    }
+
+    @When("^\"([^\"]*)\" set flags on \"([^\"]*)\" to \"([^\"]*)\"")
+    public void setFlags(String username, String message, List<String> keywords) throws Throwable {
+        userStepdefs.execWithUser(username, () -> setFlags(message, keywords));
     }
 
     @When("^the user set flags on \"([^\"]*)\" to \"([^\"]*)\"")
     public void setFlags(String message, List<String> keywords) throws Throwable {
-        String username = userStepdefs.getConnectedUser();
         MessageId messageId = getMessagesMethodStepdefs.getMessageId(message);
         String keywordString = keywords
             .stream()
             .map(value -> "\"" + value + "\" : true")
             .collect(Collectors.joining(","));
 
-        Request.Post(mainStepdefs.baseUri().setPath("/jmap").build())
-            .addHeader("Authorization", userStepdefs.getTokenForUser(username).serialize())
-            .bodyString("[" +
-                "  [" +
-                "    \"setMessages\","+
-                "    {" +
-                "      \"update\": { \"" + messageId.serialize() + "\" : {" +
-                "        \"keywords\": {" + keywordString + "}" +
-                "      }}" +
-                "    }," +
-                "    \"#0\"" +
-                "  ]" +
-                "]",
-                ContentType.APPLICATION_JSON)
-            .execute()
-            .discardContent();
+        post("[" +
+            "  [" +
+            "    \"setMessages\","+
+            "    {" +
+            "      \"update\": { \"" + messageId.serialize() + "\" : {" +
+            "        \"keywords\": {" + keywordString + "}" +
+            "      }}" +
+            "    }," +
+            "    \"#0\"" +
+            "  ]" +
+            "]");
         mainStepdefs.awaitMethod.run();
+    }
+
+    @Then("^the message \"([^\"]*)\" is not updated$")
+    public void assertIdOfTheFirstMessage(String messageName) throws Exception {
+        MessageId id = getMessagesMethodStepdefs.getMessageId(messageName);
+        System.out.println(jsonPath);
+        assertThat(jsonPath.<Map<String, String>>read("[0][1].notUpdated"))
+            .containsOnlyKeys(id.serialize());
+    }
+
+    private void post(String requestBody) throws Exception {
+        response = Request.Post(mainStepdefs.baseUri().setPath("/jmap").build())
+            .addHeader("Authorization", userStepdefs.getTokenForUser(userStepdefs.getConnectedUser()).serialize())
+            .addHeader("Accept", org.apache.http.entity.ContentType.APPLICATION_JSON.getMimeType())
+            .bodyString(requestBody, org.apache.http.entity.ContentType.APPLICATION_JSON)
+            .execute()
+            .returnResponse();
+        jsonPath = JsonPath.using(Configuration.defaultConfiguration()
+            .addOptions(Option.SUPPRESS_EXCEPTIONS))
+            .parse(response.getEntity().getContent());
     }
 }
---
+++ server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/resources/cucumber/SetMessages.feature
@@ -0,0 +1,88 @@
+#***************************************************************
+# Licensed to the Apache Software Foundation (ASF) under one   *
+# or more contributor license agreements.  See the NOTICE file *
+# distributed with this work for additional information        *
+# regarding copyright ownership.  The ASF licenses this file   *
+# to you under the Apache License, Version 2.0 (the            *
+# "License"); you may not use this file except in compliance   *
+# with the License.  You may obtain a copy of the License at   *
+#                                                              *
+#   http://www.apache.org/licenses/LICENSE-2.0                 *
+#                                                              *
+# Unless required by applicable law or agreed to in writing,   *
+# software distributed under the License is distributed on an  *
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+# KIND, either express or implied.  See the License for the    *
+# specific language governing permissions and limitations      *
+# under the License.                                           *
+# **************************************************************/
+
+Feature: GetMessages method
+  As a James user
+  I want to be able to retrieve my messages
+
+  Background:
+    Given a domain named "domain.tld"
+    And "bob@domain.tld" has a mailbox "shared"
+    And "alice@domain.tld" has a mailbox "INBOX"
+    And a user "bob@domain.tld"
+    And "bob@domain.tld" has a message "mBob" in "shared" mailbox with two attachments in text
+    And a connected user "alice@domain.tld"
+    And "alice@domain.tld" has a message "mAlice" in "INBOX" mailbox with two attachments in text
+
+# Set in mailboxes
+
+  Scenario: When allowed SetMessages can copy messages from shared mailbox
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "lr"
+    And "alice@domain.tld" copy "mBob" from mailbox "shared" of user "bob@domain.tld" to mailbox "INBOX" of user "alice@domain.tld"
+    When "alice@domain.tld" ask for messages "mBob"
+    Then the list should contain 1 message
+    And the id of the message is "mBob"
+    And the message is in "alice@domain.tld:INBOX,bob@domain.tld:shared" user mailboxes
+
+  Scenario: When allowed SetMessages can move messages out of shared mailbox
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "lrte"
+    And "alice@domain.tld" move "mBob" to mailbox "INBOX" of user "alice@domain.tld"
+    When "alice@domain.tld" ask for messages "mBob"
+    Then the list should contain 1 message
+    And the id of the message is "mBob"
+    And the message is in "alice@domain.tld:INBOX" user mailboxes
+
+  Scenario: When allowed SetMessages can add messages to a shared mailbox
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "lri"
+    And "alice@domain.tld" copy "mAlice" from mailbox "INBOX" of user "alice@domain.tld" to mailbox "shared" of user "bob@domain.tld"
+    When "alice@domain.tld" ask for messages "mAlice"
+    Then the list should contain 1 message
+    And the id of the message is "mAlice"
+    And the message is in "alice@domain.tld:INBOX,bob@domain.tld:shared" user mailboxes
+
+  Scenario: When not allowed SetMessages can not copy messages from shared mailbox
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "litewsa"
+    When "alice@domain.tld" copy "mBob" from mailbox "shared" of user "bob@domain.tld" to mailbox "INBOX" of user "alice@domain.tld"
+    Then the message "mBob" is not updated
+
+  Scenario: When not allowed SetMessages can not copy messages to shared mailbox
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "lrtewsa"
+    When "alice@domain.tld" copy "mAlice" from mailbox "INBOX" of user "alice@domain.tld" to mailbox "shared" of user "bob@domain.tld"
+    Then the message "mAlice" is not updated
+
+  Scenario: When not allowed SetMessages can not move messages out of shared mailbox
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "lriwsa"
+    When "alice@domain.tld" move "mBob" to mailbox "INBOX" of user "alice@domain.tld"
+    Then the message "mBob" is not updated
+
+# Flags update
+
+  Scenario: When allowed SetMessages can update delegated message flags
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "lrw"
+    When "alice@domain.tld" set flags on "mBob" to "$Flagged"
+    When "alice@domain.tld" ask for messages "mBob"
+    Then the list should contain 1 message
+    And the id of the message is "mBob"
+    And the keywords of the message is $Flagged
+
+
+  Scenario: When not allowed SetMessages can not update delegated message flags
+    Given "bob@domain.tld" shares its mailbox "shared" with "alice@domain.tld" with rights "latires"
+    When "alice@domain.tld" set flags on "mBob" to "$Flagged"
+    Then the message "mBob" is not updated
